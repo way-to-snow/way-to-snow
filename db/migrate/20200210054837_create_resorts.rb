8d52170876880e0c1e1befa32be3690ae9cde6bb
Sam Moore <swmoore84@gmail.com>
2020-02-10 14:50:14 +0900

    created resort model

---
+++ db/migrate/20200210054837_create_resorts.rb
@@ -0,0 +1,27 @@
+class CreateResorts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :resorts do |t|
+      t.string :name
+      t.string :prefecture
+      t.string :address
+      t.string :town
+      t.string :phone_number
+      t.string :url
+      t.integer :adult_price
+      t.integer :child_price
+      t.date :season_start
+      t.date :season_end
+      t.integer :top_elevation
+      t.integer :bottom_elevation
+      t.boolean :ski_school
+      t.boolean :child_care
+      t.integer :courses
+      t.integer :lifts
+      t.integer :advanced
+      t.integer :beginner
+      t.integer :intermediate
+
+      t.timestamps
+    end
+  end
+end
