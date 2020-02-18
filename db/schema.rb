e2c4836dedc3947a40c10e376965c9276ee7a79e
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-02-18 20:52:26 +0900

    ran db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_112524) do
+ActiveRecord::Schema.define(version: 2020_02_14_035315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,16 +57,16 @@ ActiveRecord::Schema.define(version: 2020_02_18_112524) do
 
   create_table "forecasts", force: :cascade do |t|
     t.bigint "resort_id", null: false
+    t.datetime "forecast_day"
     t.integer "max_temperature"
     t.integer "min_temperature"
     t.string "wind_direction"
     t.integer "wind_speed"
     t.float "snow_amount"
     t.string "weather"
-    t.integer "rain"
+    t.float "rain"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "forecast_day"
     t.index ["resort_id"], name: "index_forecasts_on_resort_id"
   end
 
