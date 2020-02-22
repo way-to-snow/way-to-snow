655456574fbf3a25275e4203d06e6e09c4c10633
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-02-22 15:31:44 +0900

    Revert "Merge pull request #44 from nskibiak/ajax-qna-form"
    This reverts commit c3f5b26240e6138d0fd1e2298256cdf1a8775f6e, reversing
    changes made to d252ab79d25da0522484a9d4a2acbf6d7eac931e.
    
    Failed on push to Heroku

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -36,13 +36,5 @@ class ResortsController < ApplicationController
 
   def show
     @resort = Resort.find(params[:id])
-    @question = Question.new
-    @answer = Answer.new
   end
-
-
-  #   Resort.all.select do |resort|
-  #     resort.weather_reports.order('date DESC').first.snow_change.positive?
-  #   end
-  # end
 end
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,7 @@
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import "bootstrap";
+
+import "controllers";
