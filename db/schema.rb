dfc77fbd471342a9eaa953d69f5a84f171581fc4
mohan tupakula <tmskaushik@gmail.com>
2020-03-05 21:46:37 +0900

    added last name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_124700) do
+ActiveRecord::Schema.define(version: 2020_03_05_124507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_124700) do
     t.string "about"
     t.string "skill_level"
     t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
