dfc174825d372f7a84b428f10e2112a9a3110a1e
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-03-10 21:02:26 +0900

    created a recommended button with improved search filter

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -6,12 +6,25 @@ class ResortsController < ApplicationController
   end
 
   def index
-    if params[:new_snow].present?
-      # OLD SNOW-JAPAN METHOD
-      # @resorts = Resort.joins(:weather_reports).where('weather_reports.snow_change > ? AND current = ?', 0, true)
-      @resorts = Resort.joins(:weather_reports).where('weather_reports.snow_fall = ? AND current = ?', true, true)
-    elsif params[:lots_snow].present?
-      @resorts = Resort.joins(:weather_reports).where('weather_reports.snow_depth > ? AND current = ?', 99, true)
+    # '?date=0&condition=good'
+    # @resorts = Resort.joins(:weather_reports, :forecasts)
+    # if(params[:date])
+    #   date = Date.today + params[:day].to_i.days
+    #   @resorts = @resorts.where('forecasts.forecast_day::date = ?', date )
+    # end
+    # if(condition = params[:condition])
+
+    #   @resorts = @resorts.where(forecasts: { condition: condition })
+    #     if (%w(good great).include?(condition))
+    #       @resorts = @resorts.where('weather_reports.snow_depth > ?', 50)
+    #     end
+    # end
+    if params[:recommended].present?
+      @resorts = Resort.joins(:weather_reports, :forecasts)
+        .where('weather_reports.snow_depth > ?', 50)
+        .where(weather_reports: { current: true })
+        .where(forecasts: { condition: ['good', 'great'] })
+        .where('forecasts.forecast_day::date = ?', Date.today) # + 1.day
     elsif params[:favorites].present?
       @user.favorites.any? ? @resorts = @user.resorts : @resorts = Resort.all
     else
@@ -45,3 +58,12 @@ class ResortsController < ApplicationController
     @answer = Answer.new
   end
 end
+
+# OLD SEARCH QUERIES
+  # if params[:new_snow].present?
+  #   # OLD SNOW-JAPAN METHOD
+  #   @resorts = Resort.joins(:weather_reports).where('weather_reports.snow_fall = ? AND current = ?', true, true)
+  # elsif params[:lots_snow].present?
+  #   @resorts = Resort.joins(:weather_reports).where('weather_reports.snow_depth > ? AND current = ?', 99, true)
+
+
